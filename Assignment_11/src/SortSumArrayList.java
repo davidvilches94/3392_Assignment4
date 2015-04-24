246ae96d202d3bf486bcf4c0a6b5528de46b2dbb
davidvilches94 <davidvilches94@gmail.com>
2015-04-24 12:16:01 -0500

    Initial check in

--- Assignment_11/src/SortSumArrayList.java
+++ Assignment_11/src/SortSumArrayList.java
@@ -1,25 +1,45 @@
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Scanner;
-public class SortSumArrayList {
-
+public class SortSumArrayList 
+{
 	public static void main (String [] args)
 	{
-		ArrayList <String> NumList = new ArrayList<String>();
+		ArrayList <Integer> NumList = new ArrayList<Integer>();
 		
+		@SuppressWarnings("resource")
 		Scanner input = new Scanner(System.in);
 		
-		NumList.add("Enter a number: ");
+		for(int i = 0; i < 5; i++)
+		{
+			System.out.println("Please enter a number: ");
+			NumList.add(input.nextInt());
+		}
+		System.out.println("The numbers are: ");
 		
-		NumList.add("Enter a number: ");
+		for(int i = 0; i < NumList.size(); i++)
+		{
+			System.out.print(NumList.get(i) + " ");
+		}
+		System.out.println("\n");
+		System.out.println("Here are the numbers sorted:");
 		
-		NumList.add("Enter a number: ");
-		
-		NumList.add("Enter a number: ");
-		
-		NumList.add("Enter a number: ");
-		
-		for(int index = 0; index < NumList.size(); index++)
-		System.out.println(NumList.get(index));
+		Collections.sort(NumList);
 			
+		for(int counter: NumList)
+		{
+			System.out.print(counter + " ");
+		}
+		System.out.println("\n");
+		System.out.println("Here is the sum of the numbers: " + sum(NumList));
+	}
+	public static int sum(ArrayList<Integer> list)
+	{
+		int sum = 0;
+		for(int i = 0; i< list.size(); i++)
+		{
+			sum = sum + ((int) list.get(i));
+		}
+		return sum;
 	}
 }
